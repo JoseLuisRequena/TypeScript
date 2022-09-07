409d6cdedbc27f877e1f52437fc515cf130af994
unknown <jlrequenaetcheverria@gmail.com>
2022-09-07 10:14:46 +0200

    add ;

--- index.ts
+++ index.ts
@@ -12,7 +12,7 @@ interface IBooking{
     checkOut: Date,
     discount: number,
     room: IRoom
-}
+};
 
 class Room implements IRoom{
     name: string;
@@ -25,18 +25,18 @@ class Room implements IRoom{
         this.bookings = bookings;
         this.price = price;
         this.discount = discount;
-    }
+    };
 
     isOccupied(date: Date): boolean|string{
         if(this.bookings.length){
             for(let i = 0 ; i < this.bookings.length; i++){
                 if(date >= this.bookings[i].checkIn && date <= this.bookings[i].checkOut){
                     return this.bookings[i].name
-                }
-            }
-        }
+                };
+            };
+        };
         return false;
-    }
+    };
 
     occupancyPercentage(startDate: Date, endDate: Date): number{
         //poblar un array con los dias seleccionados
@@ -46,14 +46,14 @@ class Room implements IRoom{
 
             for(let day of ArrayDatesSerch){
                 this.isOccupied(day) ? daysOccupancy.push(day) : 0;
-            }
+            };
             const OccupancyPercentage = (daysOccupancy.length * 100) / ArrayDatesSerch.length;
             
             return OccupancyPercentage;
-        }
+        };
         return 0;
-    }
-}
+    };
+};
 
 class Booking implements IBooking{
     name: string;
@@ -70,15 +70,15 @@ class Booking implements IBooking{
         this.checkIn = checkIn;
         this.checkOut = checkOut;
         this.discount = discount;
-    }
+    };
 
     get fee(){
         
         return ;
         //returns the fee, including discounts on room and guest
         //devuelve la tarifa, incluidos los descuentos en la habitación y el invitado
-    }
-}
+    };
+};
 
 function totalOccupancyPercentage(rooms: Room[], startDate: Date, endDate: Date): number { 
     if(rooms.length){
@@ -88,14 +88,14 @@ function totalOccupancyPercentage(rooms: Room[], startDate: Date, endDate: Date)
         for(let room of rooms){
             for(let day of ArrayDatesSerch){
                 room.isOccupied(day) ? daysOccupancy.push(day) : 0;
-            }
-        }
+            };
+        };
         const TotalOccupancyPercentage = (daysOccupancy.length * 100) /(ArrayDatesSerch.length * rooms.length);
         
         return TotalOccupancyPercentage;
-    } 
+    };
     return 0;
-} 
+};
 
 function availableRooms(rooms: Room[], startDate: Date, endDate: Date): string[]| boolean{
     if(rooms.length){
@@ -106,17 +106,16 @@ function availableRooms(rooms: Room[], startDate: Date, endDate: Date): string[]
             let daysOccupancy: Date[] = [];
             for(let day of ArrayDatesSerch){
                 room.isOccupied(day) ? daysOccupancy.push(day) : 0;
-            }
+            };
             
             if(!daysOccupancy.length){
                 unoccupiedRooms.push(room.name);
-            }
-        }
+            };
+        };
         return unoccupiedRooms;
-    } 
+    };
     return false;
-}
-
+};
 //Calcular el rango de días a comprobar
 function arrayDate(startDate: Date, endDate: Date): Date[]{
     let ArrayDates: Date[] = [];
@@ -124,8 +123,8 @@ function arrayDate(startDate: Date, endDate: Date): Date[]{
     while(startDate < endDate){
         ArrayDates = [...ArrayDates, new Date(startDate)];
         startDate.setDate(startDate.getDate() + 1);
-    }
+    };
     return ArrayDates;
-}
+};
 
 module.exports = { Room, Booking, totalOccupancyPercentage, availableRooms };
